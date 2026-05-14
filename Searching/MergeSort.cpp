4a6cc18325b35d5a6460f5f48650bc39d3044aad
varriyaswanth2002 <yaswanthvarri.99@gmail.com>
2026-05-14 11:31:04 +0530

    Mergesort no need to pass temp from starting the size can be getten as high-low+1 or use temp.push_back(data[i])

--- Searching/MergeSort.cpp
+++ Searching/MergeSort.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 //&arr means using same arr pass by reference , temp we can pass by value but reference pass is efficient
 
-void merge(vector<int>&data,vector<int>&temp,int low,int mid,int high){
+void merge(vector<int>&data,int low,int mid,int high){
+    vector<int>temp(high-low+1);
     int i=low,j=mid+1,y=0;
     while(i<=mid && j<=high){
         if(data[i]<=data[j]){
@@ -32,18 +33,18 @@ void merge(vector<int>&data,vector<int>&temp,int low,int mid,int high){
     }
 }
 
-void mergeSort(vector<int>&data,vector<int>&temp,int low,int high){
+void mergeSort(vector<int>&data,int low,int high){
     if(low>=high) return;
     int mid=(low+high)/2;
-    mergeSort(data,temp,low,mid);
-    mergeSort(data,temp,mid+1,high);
-    merge(data,temp,low,mid,high);
+    mergeSort(data,low,mid);
+    mergeSort(data,mid+1,high);
+    merge(data,low,mid,high);
 }
 
 int main(){
     vector<int>data{12,14,112,113,15,19,100};
-    vector<int>temp(data.size());
-    mergeSort(data,temp,0,data.size()-1);
+    //vector<int>temp(data.size());
+    mergeSort(data,0,data.size()-1);
     for(auto i:data){
         cout<<i<<" ";
     }
